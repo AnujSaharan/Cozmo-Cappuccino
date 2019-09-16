54cee7a122b1022fb94202e39eec8c0d82573e21
Riya Agrawal <43094144+ragrawal4545@users.noreply.github.com>
2019-09-15 21:52:51 -0400

    Changes

--- fsm.py
+++ fsm.py
@@ -9,9 +9,8 @@ from cozmo_classifier import ImageClassifier as cozmo_classifier
 from sklearn import svm, metrics
 from skimage import io, feature, filters, exposure, color
 
-def run(sdk_conn):
+def idle_state(robot : cozmo.robot.Robot):
     
-    robot = sdk_conn.wait_for_robot()
     robot.camera.image_stream_enabled = True
     robot.camera.color_image_enabled = False
     robot.camera.enable_auto_exposure()
@@ -19,29 +18,25 @@ def run(sdk_conn):
     image_classifier = joblib.load('image_classifier.joblib')
 
     isClassifying = 1
-
     while isClassifying:
         time.sleep(.2)
         latest_image = robot.world.latest_image
         new_image = latest_image.raw_image
 
-        # robot.say_text("Image taken").wait_for_completed()
-        timestamp = datetime.datetime.now().strftime("%dT%H%M%S%f")
-        # new_image.save("./imagestream/" + "test_"  + ".bmp")
-         # data = io.imread('./imagestream/test_.bmp')
         data = np.array(new_image)
         data = np.asarray(data.astype(np.uint8))
         data = np.expand_dims(data, axis=0)
-        # print(data.shape)
+
         data = cozmo_classifier.extract_image_features(0, data)
-        print(image_classifier.predict(data))
-        robot.say_text(str(image_classifier.predict(data))).wait_for_completed()
+        label = image_classifier.predict(data);
+        print(label)
+        robot.say_text(str(label)).wait_for_completed()
 
 
 if __name__ == '__main__':
     cozmo.setup_basic_logging()
 
     try:
-        cozmo.connect(run)
+        cozmo.connect(idle_state)
     except cozmo.ConnectionError as e:
         sys.exit("A connection error occurred: %s" % e)
