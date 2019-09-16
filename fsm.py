1f7d89aa239c866d9e88b2fb092e12063316f0eb
Anuj Saharan <anujsinghsaharan@gmail.com>
2019-09-15 22:44:19 -0400

    Implements Finite State Machine

--- fsm.py
+++ fsm.py
@@ -6,37 +6,27 @@ import numpy as np
 import re
 import joblib
 from cozmo_classifier import ImageClassifier as cozmo_classifier
+from idle_state import Idle_State as idle_state
 from sklearn import svm, metrics
 from skimage import io, feature, filters, exposure, color
 
-def idle_state(robot : cozmo.robot.Robot):
-    
-    robot.camera.image_stream_enabled = True
-    robot.camera.color_image_enabled = False
-    robot.camera.enable_auto_exposure()
-    robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
-    image_classifier = joblib.load('image_classifier.joblib')
-
-    isClassifying = 1
-    while isClassifying:
-        time.sleep(.2)
-        latest_image = robot.world.latest_image
-        new_image = latest_image.raw_image
-
-        data = np.array(new_image)
-        data = np.asarray(data.astype(np.uint8))
-        data = np.expand_dims(data, axis=0)
-
-        data = cozmo_classifier.extract_image_features(0, data)
-        label = image_classifier.predict(data);
-        print(label)
-        robot.say_text(str(label)).wait_for_completed()
-
+def finite_state_machine(state):
+    states={
+    # "drone": drone_state.execute(),
+    # "inspection": inspection_state.execute(),
+    # "order",
+    # "plane",
+    # "truck",
+    # "hands",
+    # "place",
+    "none": cozmo.run_program(idle_state.execute)
+    }
+    return states.get(state, cozmo.run_program(idle_state.execute))
 
 if __name__ == '__main__':
     cozmo.setup_basic_logging()
-
     try:
-        cozmo.connect(idle_state)
+        cozmo.run_program(finite_state_machine("none"))
     except cozmo.ConnectionError as e:
         sys.exit("A connection error occurred: %s" % e)
+
---
+++ idle_state.py
@@ -0,0 +1,31 @@
+import sys
+import cozmo
+import datetime
+import time
+import numpy as np
+import re
+import joblib
+from cozmo_classifier import ImageClassifier as cozmo_classifier
+from sklearn import svm, metrics
+from skimage import io, feature, filters, exposure, color
+
+class Idle_State:
+    def execute(robot : cozmo.robot.Robot):
+        robot.camera.image_stream_enabled = True
+        robot.camera.color_image_enabled = False
+        robot.camera.enable_auto_exposure()
+        robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
+        image_classifier = joblib.load('image_classifier.joblib')
+
+        latest_image = robot.world.latest_image
+        new_image = latest_image.raw_image
+
+        data = np.array(new_image)
+        data = np.asarray(data.astype(np.uint8))
+        data = np.expand_dims(data, axis=0)
+
+        data = cozmo_classifier.extract_image_features(0, data)
+        label = image_classifier.predict(data);
+        print(label)
+        robot.say_text(str(label)).wait_for_completed()
+        # cozmo.run_program(fsm.finite_state_machine("none"))
