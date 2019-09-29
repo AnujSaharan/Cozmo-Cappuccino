a40310b4f76aa50c5163c42ceb4a9265ac9815db
Riya Agrawal <43094144+ragrawal4545@users.noreply.github.com>
2019-09-28 22:33:03 -0400

    completed entire measurement update barring prob calculation. set up method for the same

--- lab03/particle_filter.py
+++ lab03/particle_filter.py
@@ -33,7 +33,7 @@ def motion_update(particles, odom):
             dx_new, dy_new, dh_new = add_odometry_noise(odom_new, setting.ODOM_HEAD_SIGMA, setting.ODOM_TRANS_SIGMA)
             
             #increment particle x, y, h by odom deltas
-            particle_after_motion_update = Particle(x+dx_new, y+dy_new, h+dh_new)
+            particle_after_motion_update = Particle(x + dx_new, y + dy_new, h + dh_new)
 
             motion_particles.append(particle_after_motion_update)
 
@@ -65,6 +65,63 @@ def measurement_update(particles, measured_marker_list, grid):
                 after measurement update
     """
     measured_particles = []
+    
+    weight_of_particles = []
+    probability_of_particles = []
+
+    normalizing_factor = 0.0
+        
+    #calculate weights    
+    for particle in particles:
+            #if not in grid, weight = 0.0
+            if not grid.is_in(particle.x, particle.y) or not grid.is_free(particle.x, particle.y):
+                    weight_of_particles.append(0.0)
+            else:
+                    probability = calculate_probability(particle, measured_marker_list, grid)
+                    weight_of_particles.append(probability)
+                    normalizing_factor += probability
+    
+    #spurious detection rate and detection failure rate
+    if (len(particles) > len(measured_marker_list)):
+            #the number of dropped markers = number of markers seen by the particle - number of markers seen by the robot
+            dropped_markers =  len(particles) - len(measured_marker_list)
+            scale = dropped_markers * setting.DETECTION_FAILURE_RATE
+            weight_of_particles = [weight * scale for weight in weight_of_particles]
+    else:
+            spurious_markers =  len(measured_marker_list) - len(particles)
+            scale = spurious_markers * setting.SPURIOUS_DETECTION_RATE
+            weight_of_particles = [weight * scale for weight in weight_of_particles]
+            
+        
+    #normalize
+    if normalizing_factor == 0.0: 
+            # equal probabilities
+            probability_of_particles = [1 / len(weight_of_particles) for weight in weight_of_particles]
+    else: 
+            probability_of_particles = [weight / normalizing_factor for weight in weight_of_particles]
+
+    
+    #account for addition of random samples 
+    random_sample_size = int(len(particles) / 8)
+    
+    #resample
+    measured_particles = np.random.choice(particles, size=len(particles) - random_sample_size, replace=True, p=probability_of_particles)
+    #convert from numpy array to list
+    measured_particles = measured_particles.tolist()
+    
+    #add small number of random particles 
+    random_particles = Particle.create_random(count=random_sample_size, grid=grid)
+    measured_particles = measured_particles + random_particles
+
     return measured_particles
+
+# not given method
+def calculate_probability(particle, measured_marker_list, grid):
+    probability = 0.0 
+    return probability
+    
+
+
+
 
 
